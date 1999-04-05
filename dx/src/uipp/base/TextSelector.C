432fdc1210418355ecc6e9d4d02f02a5ae50e927
gda <gda>
1999-04-05 16:32:32 +0000

    copyright notice

--- dx/src/uipp/base/TextSelector.C
+++ dx/src/uipp/base/TextSelector.C
@@ -1,11 +1,6 @@
-// 
-// (C) COPYRIGHT International Business Machines Corp. 1997
-// All Rights Reserved
-// Licensed Materials - Property of IBM
-// 
-// US Government Users Restricted Rights - Use, duplication or
-// disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
-// 
+//////////////////////////////////////////////////////////////////////
+// 		DX SOURCEFILE                                       //
+//////////////////////////////////////////////////////////////////////
 
 #include <Xm/PushB.h>
 #include <Xm/Text.h>
