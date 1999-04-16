b6d4005905eea9788a97b8f2057d226a2b30d519
gda <gda>
1999-04-16 19:19:17 +0000

    Moved defines until after params w/regard to MIN, MAX

--- dx/src/uipp/base/help.h
+++ dx/src/uipp/base/help.h
@@ -3,10 +3,9 @@
 /*****************************************************************************/
 
 /*
- * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/opendx2/Repository/dx/src/uipp/base/help.h,v 1.1 1999/03/24 15:17:25 gda Exp $
+ * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/opendx2/Repository/dx/src/uipp/base/help.h,v 1.2 1999/04/16 19:19:17 gda Exp $
  */
 
-#include "defines.h"
 
 #include "history.h"
 #include "helplist.h"
@@ -14,6 +13,8 @@
 #if !defined(OS2)  && !defined(DXD_WIN)
 #include <sys/param.h>
 #endif
+
+#include "defines.h"
 
 #define SPOTFILE "spots"
 #define README_PREFIX "README"
