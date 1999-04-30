ee0552965bc4c19185965ccf8dd57719220838a1
gda <gda>
1999-04-30 21:35:18 +0000

    Use configuration header

---
+++ dx/acconfig.h
@@ -0,0 +1,37 @@
+#ifndef __DXCONFIG_H__
+#define __DXCONFIG_H__
+
+@TOP@
+
+#define DXD_VERSION_STRING      "01.01.0001"
+#define DXD_VERSION 		1
+#define DXD_RELEASE 		1
+#define DXD_MODIFICATION 	1
+#define DXD_ARCHNAME 		"none"
+#define PACKAGE 		"none"
+#define VERSION 		"none"
+#define HAS_M_PI		0
+#define HAS_M_SQRT2		0
+#define HAS_S_ISDIR		0
+#define HAS_sysmp		0
+
+
+#undef byte
+#undef float32
+#undef float64
+#undef int16
+#undef int32
+#undef int64
+#undef int8
+#undef short
+#undef ubyte
+#undef uint16
+#undef uint32
+#undef uint64
+#undef uint8
+#undef ulong
+#undef ushort
+
+@BOTTOM@
+
+#endif
