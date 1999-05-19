5847cfb22f5ff8eeaa12858ade1634a062513ff2
gda <gda>
1999-05-19 19:46:28 +0000

    needed to cover h file HAVE's after switching from AC_CHECK_HEADERS to DX_CHECK_HEADERS

--- dx/acconfig.h
+++ dx/acconfig.h
@@ -21,12 +21,28 @@
 #define HAS_M_SQRT2		0
 #define HAS_S_ISDIR		0
 #define HAS_sysmp		0
-
 #undef HAVE_REGCMP
-
 #define USING_LESSTIF 0
-
-
+#define HAVE_FCNTL_H 0
+#define HAVE_LIMITS_H 0
+#define HAVE_MALLOC_H 0
+#define HAVE_STRINGS_H 1
+#define HAVE_SYS_FILE_H 1
+#define HAVE_SYS_IOCTL_H 1
+#define HAVE_SYS_TIME_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_SYSCONFIG_H 1
+#define HAVE_SYS_UN_H 1
+#define HAVE_VALUES_H 1
+#define HAVE_WAIT_H 1
+#define HAVE_SYS_UTSNAME_H 1
+#define HAVE_SIGNAL_H 1
+#define HAVE_SYS_SIGNAL_H 1
+#define HAVE_PROCESS_H 1
+#define HAVE_SYS_FILIO_H 1
+#define HAVE_CC_OSFCN_H 1
+#define HAVE_CC_LIBC_H 1
 #undef uint
 #undef byte
 #undef float32
