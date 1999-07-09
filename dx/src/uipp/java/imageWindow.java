a61b268db0a188da696c1a82b36c86205882cf0d
daniel <daniel>
1999-07-09 17:27:26 +0000

    pdk adding java infrastructure... martin rearranged these files here

---
+++ dx/src/uipp/java/imageWindow.java
@@ -0,0 +1,26 @@
+//////////////////////////////////////////////////////////////////////////////
+//                        I.B.M. CONFIDENTIAL                               //
+//      xxxx-yyy (c) COPYRIGHT IBM CORP. 1992   ALL RIGHTS RESERVED.        //
+//                                                                          //
+//    (I.B.M. CONFIDENTIAL-RESTRICTED when combined with the aggregated     //
+//                 OCO source materials for this program.)                  //
+//                        OCO SOURCE MATERIAL                               //
+//                   LICENSED MATERIALS-PROPERTY OF IBM.                    //
+//                                                                          //
+//////////////////////////////////////////////////////////////////////////////
+
+/*
+ * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/opendx2/Repository/dx/src/uipp/java/imageWindow.java,v 1.1 1999/07/09 17:27:26 daniel Exp $
+ */
+
+
+import dx.client.*;
+import dx.net.*;
+import java.applet.*;
+
+public class imageWindow extends ImageWindow {
+
+    public void start() {
+	super.start();
+    }
+} // end imageWindow
