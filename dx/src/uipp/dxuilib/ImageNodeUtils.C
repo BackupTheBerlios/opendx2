612433b09c5b11e48824f3236aea38955fe20aed
gda <gda>
1999-07-15 17:21:53 +0000

    BSIZE conflicted on AIX

--- dx/src/uipp/dxuilib/ImageNodeUtils.C
+++ dx/src/uipp/dxuilib/ImageNodeUtils.C
@@ -1300,12 +1300,12 @@ boolean ImageNode::SendString(void* callbackData, PacketIFCallback cb, FILE* f, 
 void ImageNode::FormatMacro (FILE* f, PacketIFCallback cb, void* cbd, String mac[], boolean viasocket)
 {
     int i = 0;
-#   define BSIZE 90
-    char tmpbuf[BSIZE];
+#   define TMPSIZE 90
+    char tmpbuf[TMPSIZE];
     int buflen = 0;
     while (mac[i]) {
 	int length = strlen(mac[i]);
-	if ((buflen + length) >= BSIZE) {
+	if ((buflen + length) >= TMPSIZE) {
 	    if (buflen > 0) {
 		tmpbuf[buflen++] = '\n'; tmpbuf[buflen] = '\0';
 		SendString (cbd, cb, f, tmpbuf, viasocket);
