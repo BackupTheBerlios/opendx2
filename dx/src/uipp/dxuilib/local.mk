a1194e112ca89c0b63f301c5b61f838df32ab188
gda <gda>
2000-05-17 19:09:34 +0000

    sh net2c to avoid permission problems

--- dx/src/uipp/dxuilib/local.mk
+++ dx/src/uipp/dxuilib/local.mk
@@ -5,19 +5,19 @@ ImageNodeUtils.o: imagemac.h dxmac.h vrmlmac.h gifmac.h
 
 
 sequence.h: sequence.net 
-	net2c sequence.net sequence.h
+	sh net2c sequence.net sequence.h
 
 imagemac.h: imagemac.net
-	net2c imagemac.net imagemac.h
+	sh net2c imagemac.net imagemac.h
 
 gifmac.h:  
-	net2c ../java/server/macros/gifmac.net gifmac.h
+	sh net2c ../java/server/macros/gifmac.net gifmac.h
 
 vrmlmac.h:
-	net2c ../java/server/macros/vrmlmac.net vrmlmac.h
+	sh net2c ../java/server/macros/vrmlmac.net vrmlmac.h
 
 dxmac.h:
-	net2c ../java/server/macros/dxmac.net dxmac.h
+	sh net2c ../java/server/macros/dxmac.net dxmac.h
 
 netlex.c: net.lex
 	$(LEX) net.lex
