3f26fb504e14c1c101d93192d199ea62bc05f27b
gda <gda>
2000-05-17 12:56:09 +0000

    Changes to compile using MS compilers under Cygwin

--- dx/src/exec/libdx/local.mk
+++ dx/src/exec/libdx/local.mk
@@ -10,7 +10,7 @@
 
 buffer.c: buffer.Z
 	sed -e 's/[ 	][ 	]*\$$/ $$/' buffer.Z > __buffer.c
-	/projects/dxcygms/windows/cpp __buffer.c \
+	/projects/dx/windows/cpp __buffer.c \
 	| tr -s "\044" "\012" \
 	| sed   -e '/^#/d' \
 		-e 's/INCLUDE2/#include/' \
@@ -19,7 +19,7 @@ buffer.c: buffer.Z
 
 shade.c: shade.Z
 	sed -e 's/[ 	][ 	]*\$$/ $$/' shade.Z > __shade_.c
-	/projects/dxcygms/windows/cpp __shade_.c  \
+	/projects/dx/windows/cpp __shade_.c  \
 	| tr -s "\044" "\012" \
 	| sed -e '/^#/d' \
 		-e 's/DEFINE2/#define/'   \
@@ -29,15 +29,15 @@ shade.c: shade.Z
 
 triangle.c: triangle.Z
 	sed -e 's/[ 	][ 	]*\$$/ $$/' triangle.Z > __triangle.c
-	/projects/dxcygms/windows/cpp -DPASS1 __triangle.c \
+	/projects/dx/windows/cpp -DPASS1 __triangle.c \
 		| sed -e '/^#/d' \
 		      -e 's/INCLUDE1/#include/' \
 		| tr -s "\044" "\012" > triangle1.c
-	/projects/dxcygms/windows/cpp -DPASS2 triangle1.c \
+	/projects/dx/windows/cpp -DPASS2 triangle1.c \
 		| sed -e '/^#/d' \
 		      -e 's/INCLUDE2/#include/' \
 		| tr -s "\044" "\012" > triangle2.c
-	/projects/dxcygms/windows/cpp -DPASS3 triangle2.c \
+	/projects/dx/windows/cpp -DPASS3 triangle2.c \
 		| tr -s "\044" "\012" \
 		| sed -e '/^#/d' \
 		      -e 's/DEFINE3/#define/' \
@@ -47,15 +47,15 @@ triangle.c: triangle.Z
 
 quad.c: quad.Z
 	sed -e 's/[ 	][ 	]*\$$/ $$/' quad.Z > __quad.c
-	/projects/dxcygms/windows/cpp -DPASS1 __quad.c \
+	/projects/dx/windows/cpp -DPASS1 __quad.c \
 		| sed -e '/^#/d' \
 		      -e 's/INCLUDE1/#include/' \
 		| tr -s "\044" "\012" > quad1.c
-	/projects/dxcygms/windows/cpp -DPASS2 quad1.c \
+	/projects/dx/windows/cpp -DPASS2 quad1.c \
 		| sed -e '/^#/d' \
 		      -e 's/INCLUDE2/#include/' \
 		| tr -s "\044" "\012" > quad2.c
-	/projects/dxcygms/windows/cpp -DPASS3 quad2.c \
+	/projects/dx/windows/cpp -DPASS3 quad2.c \
 		| tr -s "\044" "\012" \
 		| sed -e '/^#/d' \
 		      -e 's/DEFINE3/#define/' \
@@ -65,7 +65,7 @@ quad.c: quad.Z
 
 plane.c: plane.Z
 	cp plane.Z _plane.c
-	/projects/dxcygms/windows/cpp _plane.c \
+	/projects/dx/windows/cpp _plane.c \
 	        | tr -s "\044" "\012" \
 	        | sed -e '/^#/d' \
 		      -e 's/INCLUDE2/#include/' \
