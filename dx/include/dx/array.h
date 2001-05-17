7dc7a82b0bbbe34fb5e1059212d60be5618ab28a
gda <gda>
2001-05-17 20:14:13 +0000

    Exposed (eg. renamed _dxf -> DX) shared-memory array routiunes.  Also a fix to one of the procedures.

--- dx/include/dx/array.h
+++ dx/include/dx/array.h
@@ -482,6 +482,8 @@ Error 	    DXRegisterSharedSegment(int id,
 
 SharedArray DXNewSharedArray(int id, Pointer d, Type t, Category c, int r, ...);
 SharedArray DXNewSharedArrayV(int id, Pointer d, Type t, Category c, int r, int *s);
+SharedArray DXNewSharedArrayFromOffset(int id, long offset, Type t, Category c, int r, ...);
+SharedArray DXNewSharedArrayFromOffsetV(int id, long offset, Type t, Category c, int r, int *s);
 
 /* make string list arrays */
 Array DXMakeStringList(int n, char *s, ...);
