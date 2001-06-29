3d8ba1ae9c72c150e3dfb330056366b4f48274e5
gda <gda>
2001-06-29 13:10:24 +0000

    Moved structure def'ns and procedure templates to dxstereo.h so that custom stereo code can be implemented without drawing in all of the OpenDX headers.

--- dx/src/exec/hwrender/hwStereo.h
+++ dx/src/exec/hwrender/hwStereo.h
@@ -10,52 +10,13 @@
 
 #include <dxconfig.h>
 
-
 #define DX_STEREO_LEFT  1
 #define DX_STEREO_RIGHT 2
 
-typedef struct
-{
-    int xOffset, yOffset;
-    int xSize, ySize;
-    float aspect;
-} WindowInfo;
-    
-typedef int   (*InitializeStereoSystemMode)(Display *, Window);
-typedef int   (*CreateStereoWindows)(Display *, Window,
-				Window *, WindowInfo *,
-				Window *, WindowInfo *);
-typedef int   (*ExitStereo)(Display *, Window, Window, Window);
-
-
-typedef struct 
-{
-    InitializeStereoSystemMode 	initializeStereoSystemMode;
-    CreateStereoWindows 	createStereoWindows;
-    ExitStereo          	exitStereo;
-} StereoSystemMode;
+#include <dxstereo.h>
 
 Error _dxfInitializeStereoSystemMode(void *, dxObject);
 Error _dxfExitStereoSystemMode(void *);
-
-typedef void *(*InitializeStereoCameraMode)(void *, dxObject);
-typedef int   (*ExitStereoCameraMode)(void *);
-typedef int   (*CreateStereoCameras)(void *,
-				int, float, float,
-				float *, float *, float *, float, float,
-				float *, float *, float *, float **,
-				float *, float *, float *, float **);
-typedef int   (*MapStereoXY)(void *, Window, Window, WindowInfo, 
-				int, int, int*, int*);
-
-typedef struct 
-{
-    InitializeStereoCameraMode  initializeStereoCameraMode;
-    ExitStereoCameraMode  	exitStereoCameraMode;
-    CreateStereoCameras     	createStereoCameras;
-    MapStereoXY			mapStereoXY;
-} StereoCameraMode;
-
 
 Error _dxfInitializeStereoCameraMode(void *, dxObject);
 Error _dxfExitStereoCameraMode(void *);
