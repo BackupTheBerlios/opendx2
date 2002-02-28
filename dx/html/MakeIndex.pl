c5d8cbb952abd83ff105f344570ea043722d3712
davidt <davidt>
2002-02-28 19:13:59 +0000

    Here are the updated refguide documents. I've done a lot to them and cleaned up a lot of syntax. No new added information. The search engine is now giving priority to documents with terms in their Titles.

--- dx/html/MakeIndex.pl
+++ dx/html/MakeIndex.pl
@@ -3,7 +3,24 @@
 use strict;
 use warnings;
 
+# -----------------------------------------------------------------
+# Index building perl script for DX web pages.
+#
+# The following perl modules must be installed to use:
+#      HTML-Index, HTML-Tagset, HTML-Parser, HTML-Tree
+#
+# To run this to build a new index, issue the following command in
+# the root html directory:
+#       perl MakeIndex.pl `find . -name "*.htm"` > srchidx.js
+#
+# The resultant file is a giant javascript string list consisting of
+# page-name|page-title|(keyword number-of-occurances,)*.
+#
+# -----------------------------------------------------------------
 use HTML::TreeBuilder;
+
+my $MINSTRLEN = 4;
+my $TITLESTRVAL = 500;
 
 my @commonList = <DATA>;
 chomp @commonList;
@@ -23,14 +40,20 @@ foreach my $fileName (@files) {
     $titlestr[0] =~ s/,/ /g;
     my @text = $tree->as_text();
     foreach my $sym (@text) {
+    
+        # turn all non-text based characters to space
 	$sym =~ s/[^a-zA-Z0-9\-_]/ /g;
-	$sym =~ s/--/ /g;
-#	$sym =~ s/([b-df-hj-np-rtv-z])s($|[\s\n])/$1 /g; # eliminate plurals
+	
+	$sym =~ s/--/ /g; # turn hiphen into space
+
+	# Now create an array with all words >= $MINSTRLEN letters 
+	# in length and uniquify them.
+	
 	my @symar = split(" ", $sym);
 	foreach my $sym2 (@symar) {
-	    if(length($sym2) > 3) {
+	    if(length($sym2) >= $MINSTRLEN) {
 		$sym2 = lc($sym2);
-		# don't add too common words like contents
+		# don't add too common words from data past __END__
 		my $test = 1;
 		foreach my $cw (@commonList) {
 		    if($sym2 eq $cw) {
@@ -40,6 +63,27 @@ foreach my $fileName (@files) {
 		}
 		if($test) {
 		    $uniqueWords{$sym2}++;
+		}
+	    }
+	}
+	
+	# Now add the Title's key words, they get $TITLESTRVAL
+	# times the value as the words in the text.
+	
+	@symar = split(" ", $titlestr[0]);
+	foreach my $sym2 (@symar) {
+	    if(length($sym2) >= $MINSTRLEN) {
+		$sym2 = lc($sym2);
+		# don't add too common words from data past __END__
+		my $test = 1;
+		foreach my $cw (@commonList) {
+		    if($sym2 eq $cw) {
+			$test = 0;
+			last;
+		    }
+		}
+		if($test) {
+		    $uniqueWords{$sym2} += $TITLESTRVAL;
 		}
 	    }
 	}	
