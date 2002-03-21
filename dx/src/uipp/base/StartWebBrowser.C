8314f3550f940c29011988debf718eafdf021c32
davidt <davidt>
2002-03-21 19:59:20 +0000

    Minor cleanups.

--- dx/src/uipp/base/StartWebBrowser.C
+++ dx/src/uipp/base/StartWebBrowser.C
@@ -39,50 +39,49 @@
 #endif
 
 static int browserPID=0;
-static int fd=0;
 
 #if defined (intelnt)
 
 #if 0
 BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lparam) {
-   DWORD wndPid;
+    DWORD wndPid;
 
-   GetWindowThreadProcessId(hwnd, &wndPid);
-   if( wndPid == (DWORD)lparam ) {
-//	fprintf(stderr, "Found process--rising\n");
-	SetForegroundWindow( hwnd );
-	SetActiveWindow( hwnd );
-	return false;
-   }
-   else
-	return true;
+    GetWindowThreadProcessId(hwnd, &wndPid);
+    if( wndPid == (DWORD)lparam ) {
+        //	fprintf(stderr, "Found process--rising\n");
+        SetForegroundWindow( hwnd );
+        SetActiveWindow( hwnd );
+        return false;
+    }
+    else
+        return true;
 }
 #endif
 
 int ExecuteCommand(char *cmd,int nCmdShow)
 {
-   STARTUPINFO startInfo;
-   int processStarted;
-   unsigned long result;
-   PROCESS_INFORMATION pi;
-   char msgbuf[4096];
+    STARTUPINFO startInfo;
+    int processStarted;
+    unsigned long result;
+    PROCESS_INFORMATION pi;
+    char msgbuf[4096];
 
-   memset(&startInfo, 0, sizeof(STARTUPINFO));
-   startInfo.cb = sizeof(STARTUPINFO);
-   startInfo.dwFlags = STARTF_USESHOWWINDOW;
-   startInfo.wShowWindow = nCmdShow;
-   processStarted = CreateProcess(NULL,cmd,NULL,NULL,0,
-      NORMAL_PRIORITY_CLASS,
-      NULL,NULL, &startInfo, &pi);
-   if (processStarted == 0)
-      return -1;
-   WaitForInputIdle(pi.hProcess, INFINITE);
-//   AttachThreadInput( pi.dwThreadId, GetCurrentThreadId(), TRUE);
-//   EnumWindows ( EnumWindowsProc, (LPARAM) pi.dwProcessId ); 
-   result = 0;
-   CloseHandle(pi.hProcess);
-   CloseHandle(pi.hThread);
-   return result;
+    memset(&startInfo, 0, sizeof(STARTUPINFO));
+    startInfo.cb = sizeof(STARTUPINFO);
+    startInfo.dwFlags = STARTF_USESHOWWINDOW;
+    startInfo.wShowWindow = nCmdShow;
+    processStarted = CreateProcess(NULL,cmd,NULL,NULL,0,
+                                   NORMAL_PRIORITY_CLASS,
+                                   NULL,NULL, &startInfo, &pi);
+    if (processStarted == 0)
+        return -1;
+    WaitForInputIdle(pi.hProcess, INFINITE);
+    //   AttachThreadInput( pi.dwThreadId, GetCurrentThreadId(), TRUE);
+    //   EnumWindows ( EnumWindowsProc, (LPARAM) pi.dwProcessId );
+    result = 0;
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+    return result;
 
 }
 #endif
@@ -91,97 +90,98 @@ int _dxf_StartWebBrowserWithURL(char *URL) {
     char *webApp = getenv("DX_WEB_BROWSER");
 
 #if defined(macos)
-   if(webApp) {
-   	OSStatus oss;
-   	CFURLRef outAppURL;
-   	oss = LSGetApplicationForInfo(kLSUnknownType, kLSUnknownCreator, CFSTR("html"),
-   		kLSRolesViewer, NULL, &outAppURL);
-	CFStringRef urlStr = CFStringCreateWithCString(NULL, URL, kCFStringEncodingUTF8);
-	CFURLRef inURL = CFURLCreateWithString(NULL, urlStr, NULL);
-	CFArrayRef arRef = CFArrayCreate(NULL, (const void**)&inURL, 1, NULL);
-   	LSLaunchURLSpec lurl = { outAppURL, arRef, NULL, kLSLaunchDefaults, NULL};
-	oss = LSOpenFromURLSpec ( &lurl , NULL );
-	CFRelease(urlStr);
-	CFRelease(inURL);
-	CFRelease(arRef);
-	return !oss;
+    if(webApp) {
+        OSStatus oss;
+        CFURLRef outAppURL;
+        oss = LSGetApplicationForInfo(kLSUnknownType, kLSUnknownCreator, CFSTR("html"),
+                                      kLSRolesViewer, NULL, &outAppURL);
+        CFStringRef urlStr = CFStringCreateWithCString(NULL, URL, kCFStringEncodingUTF8);
+        CFURLRef inURL = CFURLCreateWithString(NULL, urlStr, NULL);
+        CFArrayRef arRef = CFArrayCreate(NULL, (const void**)&inURL, 1, NULL);
+        LSLaunchURLSpec lurl = { outAppURL, arRef, NULL, kLSLaunchDefaults, NULL};
+        oss = LSOpenFromURLSpec ( &lurl , NULL );
+        CFRelease(urlStr);
+        CFRelease(inURL);
+        CFRelease(arRef);
+        return !oss;
     }
     return 0;
 #elif defined(intelnt)
     #define MAXPATH 4096
     if(webApp) {
-	FILE *f;
-	char CmdLine[MAXPATH];
-	char fname[MAXPATH];
-	char *p;
+        FILE *f;
+        char CmdLine[MAXPATH];
+        char fname[MAXPATH];
+        char *p;
 
-	GetTempPath(MAXPATH, fname);
-	if (p = strrchr(fname, '\\'))
-	    *p = 0;
-	sprintf(fname, "%s%s.%s", fname, tmpnam(NULL), ".htm");
-	f = fopen(fname,"wb");
+        GetTempPath(MAXPATH, fname);
+        if (p = strrchr(fname, '\\'))
+            *p = 0;
+        sprintf(fname, "%s%s.%s", fname, tmpnam(NULL), ".htm");
+        f = fopen(fname,"wb");
 
-	if (f == NULL)
-	    return 0;
-	fwrite((char *)"tempfile",1,8,f);
-	fclose(f);
+        if (f == NULL)
+            return 0;
+        fwrite((char *)"tempfile",1,8,f);
+        fclose(f);
 
-	HINSTANCE hinst = FindExecutable(fname, NULL, CmdLine);
-	if ((int)hinst > 32) {
-	    strcat(CmdLine, " ");
-	    strcat(CmdLine, URL);
-	    ExecuteCommand(CmdLine, SW_SHOWNORMAL);
-//	    ShellExecute(NULL, "open", CmdLine, URL, NULL, SW_SHOWNORMAL);
-	}
-	unlink(fname);
-    	return hinst > (HINSTANCE)32;
+        HINSTANCE hinst = FindExecutable(fname, NULL, CmdLine);
+        if ((int)hinst > 32) {
+            strcat(CmdLine, " ");
+            strcat(CmdLine, URL);
+            ExecuteCommand(CmdLine, SW_SHOWNORMAL);
+            //	    ShellExecute(NULL, "open", CmdLine, URL, NULL, SW_SHOWNORMAL);
+        }
+        unlink(fname);
+        return hinst > (HINSTANCE)32;
     }
     return 0;
 #else
     if(browserPID != 0) {
-	if(waitpid(browserPID, NULL, WNOHANG)==browserPID) {
-		browserPID = 0;
-	} else {
-	    int child = fork();
-	    if (child == 0) {
-		if(webApp) {
-			char *openURL = new char[strlen(URL) + 25];
-			//strcpy(openURL, "-remote ");
-			strcpy(openURL, "openURL(");
-			strcat(openURL, URL);
-			strcat(openURL, ")");
-			int ret = execlp(webApp, webApp, "-raise", "-remote", openURL, NULL);
-            		if (!ret) fprintf(stderr, "Unable to start web browser.\n");
-			delete openURL;
-			exit (0);
-		}
-	    } else if (child > 0) {
-		return 1;
-	    } else {
-		fprintf(stderr, "Unable to fork child.\n");
-		return 0;
-	    }
-	}
+        if(waitpid(browserPID, NULL, WNOHANG)==browserPID) {
+            browserPID = 0;
+        } else {
+            int child = fork();
+            if (child == 0) {
+                if(webApp) {
+                    char *openURL = new char[strlen(URL) + 25];
+                    //strcpy(openURL, "-remote ");
+                    strcpy(openURL, "openURL(");
+                    strcat(openURL, URL);
+                    strcat(openURL, ")");
+                    int ret = execlp(webApp, webApp, "-raise", "-remote", openURL, NULL);
+                    if (!ret) fprintf(stderr, "Unable to start web browser.\n");
+                    delete openURL;
+                    exit (0);
+                }
+            } else if (child > 0) {
+                return 1;
+            } else {
+                fprintf(stderr, "Unable to fork child.\n");
+                return 0;
+            }
+        }
     }
     int child = fork();
     if (child == 0) {
-	if(webApp) {
-    	    int ret = execlp(webApp, webApp, URL, NULL);
-	    if (!ret) fprintf(stderr, "Unable to start web browser.\n");
-	    exit (0);
-	}
+        if(webApp) {
+            int ret = execlp(webApp, webApp, URL, NULL);
+            if (!ret) fprintf(stderr, "Unable to start web browser.\n");
+            exit (0);
+        }
     } else if (child > 0) {
-	sleep (2);
-	browserPID = child;
-	// Now check to see if child exists, if not, probably no browser.
-	if(waitpid(child, NULL, WNOHANG)==child)
-		return 0;
-	else
-		return 1;
+        sleep (2);
+        browserPID = child;
+        // Now check to see if child exists, if not, probably no browser.
+        if(waitpid(child, NULL, WNOHANG)==child)
+            return 0;
+        else
+            return 1;
     } else {
-	fprintf(stderr, "Unable to fork child.\n");
-	return 0;
+        fprintf(stderr, "Unable to fork child.\n");
+        return 0;
     }
+    return 0;
 #endif
 
 }
