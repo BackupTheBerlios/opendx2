f8b56ca5ff83eae6220d35b5361889dac2133e0a
davidt <davidt>
2006-03-28 20:07:52 +0000

    Add HTMLDisplay class. Update DXLinkApplication to make sure that DXLOuputNodes actually get linked up to HTMLDisplay or CaptionsLabel. Make Interactors update immediately and not require a return/enter key be pressed.

--- dx/src/uipp/java/dx/net/DXLinkApplication.java
+++ dx/src/uipp/java/dx/net/DXLinkApplication.java
@@ -3,7 +3,7 @@
 //////////////////////////////////////////////////////////////////////////////
 
 /*
- * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/opendx2/Repository/dx/src/uipp/java/dx/net/DXLinkApplication.java,v 1.3 2005/12/26 21:33:43 davidt Exp $
+ * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/opendx2/Repository/dx/src/uipp/java/dx/net/DXLinkApplication.java,v 1.4 2006/03/28 20:07:52 davidt Exp $
  */
 package dx.net;
 /*
@@ -28,14 +28,14 @@ public abstract class DXLinkApplication extends DXApplication {
     //
     private String netName = null;
     public void setNetName(String net) { this.netName = net; }
-    public String getNetName() { 
-	if (this.netName == null) this.netName = getParameter("NETNAME");
-	return this.netName; 
+    public String getNetName() {
+        if (this.netName == null) this.netName = getParameter("NETNAME");
+        return this.netName;
     }
 
     private int executing;
     private Hashtable handlers;
-    protected dx.protocol.server.serverMsg getRequest() { return new startMsg(); }
+protected dx.protocol.server.serverMsg getRequest() { return new startMsg(); }
     private int java_id;
     private String output_dir;
     private String output_url;
@@ -53,27 +53,27 @@ public abstract class DXLinkApplication extends DXApplication {
     public int getJavaId() { return this.java_id; }
 
 
-    public void setJavaId(int jid) { 
-	this.java_id = jid; 
-	super.setJavaId(jid);
+    public void setJavaId(int jid) {
+        this.java_id = jid;
+        super.setJavaId(jid);
     }
 
     public DXLinkApplication() {
-	super();
-	System.out.println (formal_version_string);
-	this.java_id = 0;
-	this.executing = 0;
-	this.handlers = null;
-	this.pending_start = null;
-	this.unassociated_dxloutputs = null;
-	this.object_applets = null;
+        super();
+        System.out.println (formal_version_string);
+        this.java_id = 0;
+        this.executing = 0;
+        this.handlers = null;
+        this.pending_start = null;
+        this.unassociated_dxloutputs = null;
+        this.object_applets = null;
     }
 
 
     public void disconnect(Thread t) {
-	if (isExecuting())
-	    this.finishedExecuting();
-	super.disconnect(null);
+        if (isExecuting())
+            this.finishedExecuting();
+        super.disconnect(null);
     }
 
     //
@@ -82,106 +82,106 @@ public abstract class DXLinkApplication extends DXApplication {
     // at the end of connect.
     //
     protected DXClientThread createCommandThread() {
-	DXClientThread t = new DXLAppThread(this, is);
-	this.pending_start = t;
-	return t;
+        DXClientThread t = new DXLAppThread(this, is);
+        this.pending_start = t;
+        return t;
     }
 
     protected void connect() {
-	super.connect();
-	executing = 0;
-	if (isConnected()) {
-	    loadMsg lm = new loadMsg();
-	    lm.setProgram(this.getNetName());
-	    send (lm.toString());
-	} 
-	if (this.pending_start != null) {
-	    this.pending_start.start();
-	    this.pending_start = null;
-	}
+        super.connect();
+        executing = 0;
+        if (isConnected()) {
+            loadMsg lm = new loadMsg();
+            lm.setProgram(this.getNetName());
+            send (lm.toString());
+        }
+        if (this.pending_start != null) {
+            this.pending_start.start();
+            this.pending_start = null;
+        }
     }
 
     public synchronized void DXLExecuteOnce() {
-	if (isConnected()) {
-	    SequencerNode sn = this.getSequencerNode();
-	    executing++;
-	    execOnceMsg eom;
-	    if (sn != null) {
-		stepSeqMsg ssm = new stepSeqMsg();
-		ssm.setFrame(sn.getFrame());
-		ssm.setMin(sn.getMin());
-		ssm.setMax(sn.getMax());
-		ssm.setDelta(sn.getDelta());
-		ssm.setName(sn.getName());
-		ssm.setInstance(sn.getInstanceNumber());
-		eom = ssm;
-	    } else {
-		eom = new execOnceMsg();
-	    }
+        if (isConnected()) {
+            SequencerNode sn = this.getSequencerNode();
+            executing++;
+            execOnceMsg eom;
+            if (sn != null) {
+                stepSeqMsg ssm = new stepSeqMsg();
+                ssm.setFrame(sn.getFrame());
+                ssm.setMin(sn.getMin());
+                ssm.setMax(sn.getMax());
+                ssm.setDelta(sn.getDelta());
+                ssm.setName(sn.getName());
+                ssm.setInstance(sn.getInstanceNumber());
+                eom = ssm;
+            } else {
+                eom = new execOnceMsg();
+            }
 
-	    eom.setMacroName(this.getMacroName());
-	    send(eom.toString());
-	    showStatus ("IBM Visualization Data Explorer is executing...");
-	} else {
-	    showStatus ("...Data Explorer disconnected");
-	}
-	super.DXLExecuteOnce();
+            eom.setMacroName(this.getMacroName());
+            send(eom.toString());
+            showStatus ("IBM Visualization Data Explorer is executing...");
+        } else {
+            showStatus ("...Data Explorer disconnected");
+        }
+        super.DXLExecuteOnce();
     }
 
     public synchronized void finishedExecuting() {
-	executing--;
-	showStatus ("Execution Finished");
-	if (isExecuting())
-	    showStatus ("IBM Visualization Data Explorer is executing...");
-	super.finishedExecuting();
+        executing--;
+        showStatus ("Execution Finished");
+        if (isExecuting())
+            showStatus ("IBM Visualization Data Explorer is executing...");
+        super.finishedExecuting();
     }
 
     public synchronized void DXLEndExecution() {
-	super.DXLEndExecution();
-	endExecMsg eem = new endExecMsg();
-	send(eem.toString());
+        super.DXLEndExecution();
+        endExecMsg eem = new endExecMsg();
+        send(eem.toString());
     }
 
     public synchronized void DXLSend (String msg) {
-	if (isConnected()) {
-	    sendValueMsg svm = new sendValueMsg();
-	    svm.setValue(msg);
-	    send(svm.toString());
-	}
-	super.DXLSend(msg);
+        if (isConnected()) {
+            sendValueMsg svm = new sendValueMsg();
+            svm.setValue(msg);
+            send(svm.toString());
+        }
+        super.DXLSend(msg);
     }
 
     public synchronized void registerHandler (String matchStr, dx.net.PacketIF node) {
-	if (this.handlers == null)
-	    this.handlers = new Hashtable(40);
-	this.handlers.put(matchStr, (Object)node);
+        if (this.handlers == null)
+            this.handlers = new Hashtable(40);
+        this.handlers.put(matchStr, (Object)node);
     }
 
     public boolean handleMessage (threadMsg msg) {
-	if (this.handlers == null) 
-	    return false;
+        if (this.handlers == null)
+            return false;
 
-	messageMsg mmsg = (messageMsg)msg;
-	String msgTxt = mmsg.getMessage();
-	int longest = 0;
-	dx.net.PacketIF found_node = null;
-	Enumeration enum1 = this.handlers.keys();
-	while (enum1.hasMoreElements()) {
-	    String matchStr = (String)enum1.nextElement();
-	    if (msgTxt.startsWith(matchStr)) {
-		int length = matchStr.length();
-		if (length > longest) {
-		    found_node = (dx.net.PacketIF)this.handlers.get(matchStr);
-		    longest = length;
-		}
-	    }
-	}
-	if (found_node != null) {
-	    found_node.handleMessage(msgTxt);
-	    return true;
-	} else {
-	    return false;
-	}
+        messageMsg mmsg = (messageMsg)msg;
+        String msgTxt = mmsg.getMessage();
+        int longest = 0;
+        dx.net.PacketIF found_node = null;
+        Enumeration enum1 = this.handlers.keys();
+        while (enum1.hasMoreElements()) {
+            String matchStr = (String)enum1.nextElement();
+            if (msgTxt.startsWith(matchStr)) {
+                int length = matchStr.length();
+                if (length > longest) {
+                    found_node = (dx.net.PacketIF)this.handlers.get(matchStr);
+                    longest = length;
+                }
+            }
+        }
+        if (found_node != null) {
+            found_node.handleMessage(msgTxt);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     //
@@ -192,123 +192,155 @@ public abstract class DXLinkApplication extends DXApplication {
     // figuring that out.
     //
     public void start() {
-	super.start();
-	if (this.unassociated_dxloutputs == null) {
-	    try {
-		Class dc = Class.forName("dx.net.DXLOutputNode");
-		this.unassociated_dxloutputs = this.network.makeNodeList(dc, false);
-	    } catch (ClassNotFoundException cnfe) {
-	    } catch (Exception e) {
-	    }
-	}
-	if (this.unassociated_dxloutputs == null) 
-	    return ;
+        super.start();
+        if (this.unassociated_dxloutputs == null) {
+            try {
+                Class dc = Class.forName("dx.net.DXLOutputNode");
+                this.unassociated_dxloutputs = this.network.makeNodeList(dc, false);
+            } catch (ClassNotFoundException cnfe) {
+            } catch (Exception e) {
+            }
+        }
+        if (this.unassociated_dxloutputs == null)
+            return ;
 
-	if (this.unassociated_dxloutputs.size() > 0) {
-	    Class dhClass = null;
-	    try {
-		dhClass = Class.forName("dx.net.DXLinkHandler");
-	    } catch (ClassNotFoundException cnfe) {
-	    }
-	    if (dhClass == null) return ;
+        if (this.unassociated_dxloutputs.size() > 0) {
+            Class dhClass = null;
+            try {
+                dhClass = Class.forName("dx.net.DXLinkHandler");
+            } catch (ClassNotFoundException cnfe) {
+            }
+            if (dhClass == null) return ;
 
-	    Vector assd = null;
-	    Enumeration enum1 = this.unassociated_dxloutputs.elements();
-	    while (enum1.hasMoreElements()) {
-		DXLOutputNode don = (DXLOutputNode)enum1.nextElement();
-		if (don.hasHandler()) {
-		    if (assd == null)
-			assd = new Vector(4);
-		    assd.addElement(don);
-		    continue;
-		}
+            Vector assd = null;
+            Enumeration enum1 = this.unassociated_dxloutputs.elements();
+            while (enum1.hasMoreElements()) {
+                DXLOutputNode don = (DXLOutputNode)enum1.nextElement();
+                if (don.hasHandler()) {
+                    if (assd == null)
+                        assd = new Vector(4);
+                    assd.addElement(don);
+                    continue;
+                }
 
-		String key = don.getNotation();
+                String key = don.getNotation();
 
-		Enumeration appls = this.getAppletContext().getApplets();
-		while (appls.hasMoreElements()) {
-		    Applet a = (Applet)appls.nextElement();
-		    if (dhClass.isInstance(a)) {
-			DXLinkHandler dh = (DXLinkHandler)a;
-			if (dh.hasHandler(key)) {
-			    don.setOutputHandler (dh, null);
-			    if (assd == null)
-				assd = new Vector(4);
-			    assd.addElement(don);
-			}
-		    }
-		}
-	    }
+				// Here, what we should do is look through all the applets
+				// to locate one that can handle the DXLOutputNode. If we
+				// can't find one, then there is a chance that not all the
+				// applets have been loaded yet. This is a problem, because
+				// then we won't have any hookup between them. So instead
+				// what we need to do is sleep for a little bit and try
+				// locating them again. If we do this over and over eventually
+				// the applets should get loaded and we should be able to
+				// hook them up. The biggest problem is going to be if the
+				// applets don't exist for the OutputNode--then we need some
+				// way to timeout--or maybe better throw and exception.
+				
+  				boolean foundApplet = false;
+  				int numTries = 0;
+                
+                while(! foundApplet) {
+                	numTries++;
+                	
+                	Enumeration appls = this.getAppletContext().getApplets();
+                	int numApplets = 1;
+                	while (appls.hasMoreElements()) {
+                    	Applet a = (Applet)appls.nextElement();
+                		//System.err.println("looking at applet num: " + numApplets++ + "  name: " + a.getClass().getName());
+                    	if (dhClass.isInstance(a)) {
+                    	    DXLinkHandler dh = (DXLinkHandler)a;
+                        	if (dh.hasHandler(key)) {
+                            	don.setOutputHandler (dh, null);
+                            	if (assd == null)
+                                	assd = new Vector(4);
+                            	assd.addElement(don);
+                            	foundApplet = true;
+                            	break;
+                        	}
+                    	}
+                	}
+                	if(numTries > 120)
+                		throw new NullPointerException("Unable to locate DXLOutputHandler for: " + key);
+                	else
+                		try {
+                			Thread.sleep(500); // wait 1/2 seconds.
+                		} catch (Exception e) {}
+                }
+                
+                
+                
+            }
 
-	    if (assd != null) {
-		enum1 = assd.elements();
-		while (enum1.hasMoreElements()) {
-		    DXLOutputNode don = (DXLOutputNode)enum1.nextElement();
-		    this.unassociated_dxloutputs.removeElement(don);
-		}
-	    }
-	}
+            if (assd != null) {
+                enum1 = assd.elements();
+                while (enum1.hasMoreElements()) {
+                    DXLOutputNode don = (DXLOutputNode)enum1.nextElement();
+                    this.unassociated_dxloutputs.removeElement(don);
+                }
+            }
+        }
     }
 
 
     public void showObject (String name, String path, int id) {
-	if ((this.object_applets == null) || (this.object_applets.size() < 1))
-	    this.locateObjectWindows();
-	if (this.object_applets == null) return ;
-	if (this.object_applets.size() < 1) return ;
+        if ((this.object_applets == null) || (this.object_applets.size() < 1))
+            this.locateObjectWindows();
+        if (this.object_applets == null) return ;
+        if (this.object_applets.size() < 1) return ;
 
-	Enumeration enum1 = this.object_applets.elements();
-	DXViewIF destination = null;
-	while (enum1.hasMoreElements()) {
-	    DXViewIF dvi = (DXViewIF)enum1.nextElement();
+        Enumeration enum1 = this.object_applets.elements();
+        DXViewIF destination = null;
+        while (enum1.hasMoreElements()) {
+            DXViewIF dvi = (DXViewIF)enum1.nextElement();
 
-	    String nname = dvi.getNodeName();
-	    if (nname == null) continue;
-	    if (nname.equals(name)) {
-		destination = dvi;
-		break;
-	    }
-	}
-	if ((destination == null) && (id < this.object_applets.size())) 
-	    destination = (DXViewIF)this.object_applets.elementAt(id);
-	if (destination == null) 
-	    destination = (DXViewIF)this.object_applets.elementAt(0);
+            String nname = dvi.getNodeName();
+            if (nname == null) continue;
+            if (nname.equals(name)) {
+                destination = dvi;
+                break;
+            }
+        }
+        if ((destination == null) && (id < this.object_applets.size()))
+            destination = (DXViewIF)this.object_applets.elementAt(id);
+        if (destination == null)
+            destination = (DXViewIF)this.object_applets.elementAt(0);
 
-	String urlstr = null;
-	try {
-	    URL url = null;
-	    url = new URL(getCodeBase(), path);
-	    urlstr = url.toString();
-	} catch (MalformedURLException mue) {
-	    System.out.println ("ActiveXApp: cannot create url from " + path);
-	}
-	if (urlstr != null) 
-	    destination.setUrl ("!tear " + urlstr);
+        String urlstr = null;
+        try {
+            URL url = null;
+            url = new URL(getCodeBase(), path);
+            urlstr = url.toString();
+        } catch (MalformedURLException mue) {
+            System.out.println ("ActiveXApp: cannot create url from " + path);
+        }
+        if (urlstr != null)
+            destination.setUrl ("!tear " + urlstr);
     }
 
     private void locateObjectWindows() {
-	AppletContext apcxt = this.getAppletContext();
-	Class suits_if = null;
-	try {
-	    suits_if = Class.forName("dx.net.DXViewIF");
-	} catch (ClassNotFoundException cnfe) {
-	    cnfe.printStackTrace();
-	    return ;
-	}
-	if (this.object_applets == null)
-	    this.object_applets = new Vector(4);
+        AppletContext apcxt = this.getAppletContext();
+        Class suits_if = null;
+        try {
+            suits_if = Class.forName("dx.net.DXViewIF");
+        } catch (ClassNotFoundException cnfe) {
+            cnfe.printStackTrace();
+            return ;
+        }
+        if (this.object_applets == null)
+            this.object_applets = new Vector(4);
 
-	Enumeration enum1 = apcxt.getApplets();
-	while (enum1.hasMoreElements()) {
-	    Applet a = (Applet)enum1.nextElement();
-	    Class c = a.getClass();
-	    Class[] faces = c.getInterfaces();
-	    int len = faces.length;
-	    for (int i=0; i<len; i++) {
-		if (faces[i].equals(suits_if)) 
-		    this.object_applets.addElement(a);
-	    }
-	}
+        Enumeration enum1 = apcxt.getApplets();
+        while (enum1.hasMoreElements()) {
+            Applet a = (Applet)enum1.nextElement();
+            Class c = a.getClass();
+            Class[] faces = c.getInterfaces();
+            int len = faces.length;
+            for (int i=0; i<len; i++) {
+                if (faces[i].equals(suits_if))
+                    this.object_applets.addElement(a);
+            }
+        }
     }
 
 } // end DXLinkApplication
--- dx/src/uipp/java/dx/net/Node.java
+++ dx/src/uipp/java/dx/net/Node.java
@@ -5,6 +5,14 @@ package dx.net;
 import java.util.*;
 import java.applet.*;
 
+//-------------------------------------------------------
+// 3/17/2006 jer
+// Added method: setOutputValueQuietly(String)
+// This is to support Interactors that want to keep the
+// server informed of changed values without causing
+// an execution.
+//-------------------------------------------------------
+
 public class Node extends Object implements dx.net.PacketIF {
 
     private int instance;
@@ -20,66 +28,66 @@ public class Node extends Object implements dx.net.PacketIF {
     public String getNotation() { return this.notation; }
 
     public void addInputArc (int input, Node src, int output) {
-	if (this.input_arcs == null)
-	    this.input_arcs = new Vector(4);
-	if (this.isInputConnected(input) == true) return ;
-	Arc a = new Arc(src, output, this, input);
-	this.input_arcs.addElement(a);
+        if (this.input_arcs == null)
+            this.input_arcs = new Vector(4);
+        if (this.isInputConnected(input) == true) return ;
+        Arc a = new Arc(src, output, this, input);
+        this.input_arcs.addElement(a);
     }
 
     public void setInputValueString (int input, String value) {
-	if (this.input_values == null)
-	    this.input_values = new Hashtable(4);
-	//if (this.isInputConnected(input) == true) return ;
-	Integer i = new Integer(input);
-	//
-	// Can hashtable hold duplicate keys?
-	//
-	if (this.input_values.containsKey(i) == true)
-	    this.input_values.remove(i);
-	this.input_values.put(i, value);
+        if (this.input_values == null)
+            this.input_values = new Hashtable(4);
+        //if (this.isInputConnected(input) == true) return ;
+        Integer i = new Integer(input);
+        //
+        // Can hashtable hold duplicate keys?
+        //
+        if (this.input_values.containsKey(i) == true)
+            this.input_values.remove(i);
+        this.input_values.put(i, value);
     }
 
     public String getInputValueString(int input) {
-	if (this.input_values == null) return null;
-	Integer i = new Integer(input);
-	return (String)this.input_values.get(i);
+        if (this.input_values == null) return null;
+        Integer i = new Integer(input);
+        return (String)this.input_values.get(i);
     }
 
     protected Node getSourceNode(int input) {
-	if (this.input_arcs == null) return null;
-	Enumeration enum1 = this.input_arcs.elements();
-	while (enum1.hasMoreElements()) {
-	    Arc a = (Arc)enum1.nextElement();
-	    if (a.getInputParamNumber() == input) 
-		return a.getSourceNode();
-	}
-	return null;
+        if (this.input_arcs == null) return null;
+        Enumeration enum1 = this.input_arcs.elements();
+        while (enum1.hasMoreElements()) {
+            Arc a = (Arc)enum1.nextElement();
+            if (a.getInputParamNumber() == input)
+                return a.getSourceNode();
+        }
+        return null;
     }
 
     protected boolean isInputConnected(int input) {
-	if (this.input_arcs == null) return false;
-	Enumeration enum1 = this.input_arcs.elements();
-	while (enum1.hasMoreElements()) {
-	    Arc a = (Arc)enum1.nextElement();
-	    if (a.getInputParamNumber() == input) 
-		return true;
-	}
-	return false;
+        if (this.input_arcs == null) return false;
+        Enumeration enum1 = this.input_arcs.elements();
+        while (enum1.hasMoreElements()) {
+            Arc a = (Arc)enum1.nextElement();
+            if (a.getInputParamNumber() == input)
+                return true;
+        }
+        return false;
     }
 
-    public void installHandlers() { }
+public void installHandlers() { }
     public void enableJava() { this.installHandlers(); }
 
     public Node(Network net, String name, int instance, String notation) {
-	super();
-	this.instance = instance;
-	this.net = net;
-	this.name = name;
-	this.param_number = 1;
-	this.input_arcs = null;
-	this.input_values = null;
-	this.notation = notation;
+        super();
+        this.instance = instance;
+        this.net = net;
+        this.name = name;
+        this.param_number = 1;
+        this.input_arcs = null;
+        this.input_values = null;
+        this.notation = notation;
     }
 
     public String getMatchString() { return null; }
@@ -91,74 +99,92 @@ public class Node extends Object implements dx.net.PacketIF {
     public int  getInstanceNumber() { return this.instance; }
     public int  getParameterNumber() { return this.param_number; }
 
-    public void setInputValue (int param, String value) { 
-	DXLinkApplication dxa = (DXLinkApplication)this.net.getApplet();
-	String macro_name = dxa.getMacroName();
-	String assignment = macro_name + "_" + name + "_" + this.instance + 
-	    "_in_" + param + " = " + value + ";";
-	DXLSend(assignment);
+    public void setInputValue (int param, String value) {
+        DXLinkApplication dxa = (DXLinkApplication)this.net.getApplet();
+        String macro_name = dxa.getMacroName();
+        String assignment = macro_name + "_" + name + "_" + this.instance +
+                            "_in_" + param + " = " + value + ";";
+        DXLSend(assignment);
     }
 
     public void setInputValueQuietly (int param, String value) {
-	DXLinkApplication dxa = (DXLinkApplication)this.net.getApplet();
-	String macro_name = dxa.getMacroName();
-	String assignment = "Executive(\"assign noexecute\"," +
-	    "\"" + macro_name + "_" + name + "_" + this.instance + 
-		"_in_" + param + "\"," + value + ");";
-	DXLSend(assignment);
+        DXLinkApplication dxa = (DXLinkApplication)this.net.getApplet();
+        String macro_name = dxa.getMacroName();
+        String assignment = "Executive(\"assign noexecute\"," +
+                            "\"" + macro_name + "_" + name + "_" + this.instance +
+                            "_in_" + param + "\"," + value + ");";
+        DXLSend(assignment);
     }
 
-    public void setOutputValue (String value) { 
-	this.setOutputValue (this.getParameterNumber(), value);
+    public void setOutputValue (String value) {
+        this.setOutputValue (this.getParameterNumber(), value);
     }
     public void setOutputValues (String name, String value) { }
 
+    //------------------------------------------------------------------
+    // BEGIN: 3/17/2006 jer
+    public void setOutputValueQuietly(String value) {
+        this.setOutputValueQuietly (this.getParameterNumber(), value);
+    }
+
+    protected void setOutputValueQuietly (int param, String value) {
+        DXLinkApplication dxa = (DXLinkApplication)this.net.getApplet();
+        String macro_name = dxa.getMacroName();
+        String assignment = "Executive(\"assign noexecute\"," +
+                            "\"" + macro_name + "_" + name + "_" + this.instance +
+                            "_out_" + param + "\"," + value + ");";
+        DXLSend(assignment);
+    }
+    // END: 3/17/2006 jer
+    //------------------------------------------------------------------
+
     protected void setOutputValue (int param, String value) {
-	DXLinkApplication dxa = (DXLinkApplication)this.net.getApplet();
-	String macro_name = dxa.getMacroName();
-	String assignment = macro_name + "_" + name + "_" + this.instance + 
-	    "_out_" + param + " = " + value + ";";
-	DXLSend(assignment);
+        DXLinkApplication dxa = (DXLinkApplication)this.net.getApplet();
+        String macro_name = dxa.getMacroName();
+        String assignment = macro_name + "_" + name + "_" + this.instance +
+                            "_out_" + param + " = " + value + ";";
+        DXLSend(assignment);
     }
 
     protected void DXLSend(String assignment) {
-	try {
-	    DXApplication dxa = (DXApplication)this.net.getApplet();
-	    dxa.DXLSend(assignment);
-	} catch (Exception e) {
-	}
+        try {
+            DXApplication dxa = (DXApplication)this.net.getApplet();
+            dxa.DXLSend(assignment);
+        } catch (Exception e) {
+        }
     }
 
     protected void registerHandler (String matchStr) {
-	try {
-	    DXLinkApplication dxa = (DXLinkApplication)this.net.getApplet();
-	    dxa.registerHandler(matchStr, this);
-	} catch (Exception e) {
-	}
+        try {
+            DXLinkApplication dxa = (DXLinkApplication)this.net.getApplet();
+            dxa.registerHandler(matchStr, this);
+        } catch (Exception e) {
+        }
     }
 
     public void handleMessage (String msg) {
-	StringTokenizer stok = new StringTokenizer(msg, ";");
-	while (stok.hasMoreTokens()) {
-	    String tok = stok.nextToken();
-	    try {
-		StringTokenizer assign_tok = new StringTokenizer(tok, "=");
-		String lhs = assign_tok.nextToken();
-		StringTokenizer remspaces = new StringTokenizer(lhs, " ");
-		String realLhs = null;
-		while (remspaces.hasMoreTokens()) 
-		    realLhs = remspaces.nextToken();
-		lhs = realLhs;
-		String rhs = assign_tok.nextToken();
-		handleAssignment (lhs, rhs);
-	    } catch (Exception e) {
-	    }
-	}
+        StringTokenizer stok = new StringTokenizer(msg, ";");
+        while (stok.hasMoreTokens()) {
+            String tok = stok.nextToken();
+            try {
+                StringTokenizer assign_tok = new StringTokenizer(tok, "=");
+                String lhs = assign_tok.nextToken();
+                StringTokenizer remspaces = new StringTokenizer(lhs, " ");
+                String realLhs = null;
+                while (remspaces.hasMoreTokens())
+                    realLhs = remspaces.nextToken();
+                lhs = realLhs;
+                String rhs = assign_tok.nextToken();
+                handleAssignment (lhs, rhs);
+            } catch (Exception e) {
+            }
+        }
     }
 
     public boolean handleAssignment (String lhs, String rhs) {
-	//System.out.println (this.name + ": not handled: " + lhs + "=" + rhs);
-	return false;
+        //System.out.println (this.name + ": not handled: " + lhs + "=" + rhs);
+        return false;
     }
 
 }; // end Node
+
